b9f8f60aad26e3e5e878c6a83f30cd2f347a478b
Davi Gabriel <davig41441503@gmail.com>
2024-07-13 10:41:35 -0300

    project created

---
+++ src/main/java/br/org/davi/todolistwithrest/ToDoListWithRestApplication.java
@@ -0,0 +1,13 @@
+package br.org.davi.todolistwithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListWithRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListWithRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/org/davi/todolistwithrest/ToDoListWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.davi.todolistwithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListWithRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
